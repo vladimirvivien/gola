4e554810f3f9813c2c7d6d1abfae126b760230d1
Vladimir Vivien <vvivien@xor.exchange>
2015-09-04 06:53:20 -0400

    New methods for add, sub, scale.

--- vector.go
+++ vector.go
@@ -11,6 +11,12 @@ func New(elems ...float64) Vector {
 	return elems
 }
 
+func (v Vector) assertLenMatch(other Vector) {
+	if len(v) != len(other) {
+		panic("Vector length mismatch")
+	}
+}
+
 func (v Vector) String() string {
 	buff := bytes.NewBufferString("[")
 	for i, val := range v {
@@ -24,13 +30,36 @@ func (v Vector) String() string {
 }
 
 func (v Vector) Eq(other Vector) bool {
-	if len(v) != len(other) {
-		return false
-	}
+	v.assertLenMatch(other)
 	for i, val := range v {
 		if val != other[i] {
 			return false
 		}
 	}
 	return true
+}
+
+func (v Vector) Add(other Vector) (result Vector) {
+	v.assertLenMatch(other)
+	result = make([]float64, len(v))
+	for i, val := range v {
+		result[i] = val + other[i]
+	}
+	return
+}
+
+func (v Vector) Sub(other Vector) (result Vector) {
+	v.assertLenMatch(other)
+	result = make([]float64, len(v))
+	for i, val := range v {
+		result[i] = val - other[i]
+	}
+	return
+}
+
+func (v Vector) Scale(scale float64) {
+	for i := range v {
+		v[i] = v[i] * scale
+	}
+	return
 }
